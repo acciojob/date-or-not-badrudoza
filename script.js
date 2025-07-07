28b4cd59afb8193993c2d57e89d9572248abe599
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-08 01:03:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var isDate = function (input) {
   //   write your code here
-	let date=new Date(input);
-	return !isNaN(date.getTime());
+	let date = new Date(input);             // Convert to Date object
+    return !isNaN(date.getTime())
+	
 };
 
 // Do not change the code below.
-/const input = prompt("Enter Date.");
+const input = prompt("Enter Date.");
 alert(isDate(input));
